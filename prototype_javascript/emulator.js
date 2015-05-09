551ce187615093d526248497996b94c37cd5f306
Rory Mearns <r.mearnsy@gmail.com>
2015-05-09 15:27:55 +1200

    created a custom queue in case I want to add extra functionality to it. added the ability to click on the canvas and it will gather the XY coordinates of where the click was on the canvas

--- prototype_javascript/emulator.js
+++ prototype_javascript/emulator.js
@@ -1,17 +1,58 @@
 /* Emulator for 345 Assignment: Rory Mearns (ID.3928873) */
 
+/* ------ Program Variables & Setup ------ */
 
-// Canvas elemnt
+var instructionQueue; 		// Queue of incoming instructions for the app. App will poll this queue for new events
+
+
+// Canvas Element
 var canvas = $("#proto_canvas").get(0);
 var ctx = canvas.getContext("2d");	
 
 
+/* ------ Basic Queue ------ */
+
+function Queue () {
+	var items = [];
+
+	this.enqueue = function (item) {
+		items.push(item);
+	}
+	this.dequeue = function () {
+		return items.shift();
+	}
+	this.peek = function () {
+		return items[0];
+	}
+	this.isEmpty = function () {
+		if (items.length == 0) {
+			alert("it's empty");
+			return true;
+		} else {
+			alert("it's empty");
+			return false;
+		}
+	}
+};
+
+
+
+/* ------ General Functions ------ */
+
+function flag (message) {				// A testing funciton for getting things going
+
+};
+
+
+/* ------ Button & Input Listeners ------ */
+
 $(document).ready(function() {
 	$("#up").click(function() {
+
 		//alert("UP is working");
 	});
 	$("#down").click(function() {
-		//alert("DOWN is working");
+		alert("DOWN is working");
 	});
 	$("#left").click(function() {
 		//alert("LEFT is working");
@@ -22,9 +63,27 @@ $(document).ready(function() {
 	$("#enter").click(function() {
 		//alert("SELECT is working");
 	});
+	$("#proto_canvas").mousedown(function(point) {
+		var offset = $("#proto_canvas").offset();
+		var x = point.pageX-offset.left;
+		var y = point.pageY-offset.top;
+		//alert("X: "+x+" Y: "+y);
+	});
+
+
 	//ctx.fillRect(0, 0, 60, 60);
 });
 
+/* ------ Emulator Setup ------ */
+
+function initialize () {
+
+	instructionQueue = new Queue();		
+
+}
+
+
+initialize();
 
 /* 
 Functions I wand the emulator to handle:
