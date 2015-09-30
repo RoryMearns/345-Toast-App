9854a6b61b00fbfee188f79c10b53a6fa52b34a9
Rory Mearns <r.mearnsy@gmail.com>
2015-09-30 13:48:18 +1300

    Removed a few extra files and folders

--- porting files/isolated guest emulator/game.js
+++
@@ -1,9 +0,0 @@
-
-
-
-setBG("resources/bg.jpg");
-addResource('bg', 110, 110, "resources/bg.jpg")
-// addResource(name, x, y, imgFile): To allow us to add images to the emulator and paint them. Must take a name, x and y coordinates and the image filename. Returns a reference to the GameObject.
-initialize();
-start();
-paint();
